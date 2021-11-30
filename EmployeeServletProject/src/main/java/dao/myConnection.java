a7587e559d44fd2359e7f6ee255817f121552fd4
Parfait Domagni <parfaitdomagni@gmail.com>
2021-11-30 10:34:36 -0600

    First Commit

---
+++ EmployeeServletProject/src/main/java/dao/myConnection.java
@@ -0,0 +1,19 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class myConnection {
+	public static Connection getConnection() {
+	Connection con = null;
+	try{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_first", "pdomagni","Jesus2020");
+		return con;
+	} 
+	catch (Exception e) {
+		e.printStackTrace();
+	}
+	return null;
+	}
+}
---
+++ EmployeeServletProject/src/main/java/model/Employee.java
@@ -0,0 +1,66 @@
+package model;
+
+public class Employee {
+	private int id;
+	private String name;
+	private double salary;
+	private String loginId;
+	private String password;
+	public Employee() {}
+	public Employee(int id, String name, double salary, String loginId, String password) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.loginId = loginId;
+		this.password =password;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setName(String name) { 
+	this.name = name;
+	}
+	
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getSalary() {
+		return salary;
+	}
+	
+	public String getLoginId() {
+		return loginId;
+	}
+	
+	public String setPassword() {
+		return password;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", loginId=" + loginId + ", password="
+				+ password + "]";
+	}
+	
+	
+	
+
+}
